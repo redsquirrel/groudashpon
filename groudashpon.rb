7de12b77e52fbb99c3446671679daa32a5a67936
Dave Hoover <dave.hoover@gmail.com>
2010-05-27 13:34:50 -0500

    Displaying all Groupon cities

--- groudashpon.rb
+++ groudashpon.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 
+require 'city'
+
 get '/' do
-  "Hello from Sinatra on GrouDASHpon!"
+  body = "<h1>GrouDASHpon</h1>"
+  City.all.each do |city|
+    body << %Q(<h2 id="#{city['id']}">#{city['name']}</h2>)
+    # body << "<pre>#{city.inspect}</pre>"
+  end
+  body
 end
