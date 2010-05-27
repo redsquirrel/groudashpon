e2e6b5dbd723960893018222c65d32be03b8a8c4
Dave Hoover <dave.hoover@gmail.com>
2010-05-27 13:37:20 -0500

    Oops, forgot to add the new files :)

---
+++ city.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+require 'json'
+
+class City
+  URL = URI.parse("http://www.groupon.com/api/v1/divisions.json")
+  
+  def self.all
+    response = Net::HTTP.start(URL.host, URL.port) do |http|
+      http.get(URL.path)
+    end
+    
+    JSON.parse(response.body)["divisions"]
+  end
+end
+
---
+++ deal.rb
@@ -0,0 +1,8 @@
+require 'net/http'
+require 'json'
+
+class Deal
+  def self.at(location)
+    
+  end
+end
