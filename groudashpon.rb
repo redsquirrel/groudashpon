4fdb94fbccab3ac7963ea60706955c3d8c05c882
Dave Hoover <dave.hoover@gmail.com>
2010-05-27 21:10:04 -0500

    Lining up the cities vertically; Then dynamically sorting big money cities on top

--- groudashpon.rb
+++ groudashpon.rb
@@ -5,11 +5,14 @@ require 'city'
 
 get '/' do
   body = "<h1>GrouDASHpon</h1>"
+  body << %Q(<ul id="cities">)
   City.all.each do |city|
+    body << %Q(<li id="#{city['id']}-item">)
     body << %Q(<a target="_blank" href="http://groupon.com/#{city['id']}">#{city['name']}</a>\n)
-    body << %Q($<span class="city-data" id="#{city['id']}"></span>&nbsp;&nbsp;\n)
+    body << %Q($<span class="city-data" id="#{city['id']}"></span>\n)
     body << city_setup(city['id'])
     # body << "<pre>#{city.inspect}</pre>"
+    body << "</li>"
   end
   body << %Q(<p><a target="_blank" href="http://github.com/redsquirrel/groudashpon">source</a></p>\n)
   "<html><head>" + head + "</head><body>" + body + "</body></html>"
@@ -21,6 +24,25 @@ def head
 <script src="http://js.pusherapp.com/1.4/pusher.min.js" type="text/javascript"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.6.1.0/prototype.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/scriptaculous/1.8.3/scriptaculous.js"></script>
+<script type="text/javascript">
+var list = {};
+function cityData(element) {
+  var city = element.id.replace("-item", "");
+  return (city && $(city).innerHTML) || 0;
+}
+
+function sortNumber(a, b) {
+  return cityData(b) - cityData(a);
+}
+
+function sortList() {
+  var sortedCities = $("cities").childElements().sort(sortNumber);
+  $("cities").innerHTML = "";
+  sortedCities.each(function(city) {
+    $("cities").appendChild(city);
+  });
+}
+</script>
 HEAD
 end
 
@@ -31,7 +53,8 @@ def city_setup(id)
   server.bind('update', function(amount) {
     if ($("#{id}").innerHTML != amount) {
       $("#{id}").update(amount);
-      new Effect.Highlight("#{id}");      
+      sortList();
+      new Effect.Highlight("#{id}");
     }
   });
 </script>
