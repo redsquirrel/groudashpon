107b672e9035e68abcb8bf3f44c1fc910631b750
Michael Hines <mike@michaelphines.net>
2010-06-04 14:21:36 -0500

    Add live-update map of purchased groupons.

--- groudashpon.rb
+++ groudashpon.rb
@@ -6,20 +6,3 @@ require 'city'
 get '/' do
   erb :index
 end
-
-helpers do
-  def city_setup(id)
-    <<-JAVASCRIPT
-    <script type="text/javascript">
-      socket.subscribe("#{id}").bind('update', function(rawAmount) {
-        var amount = currencyFormat(rawAmount); 
-        if ($("#{id}").innerHTML != amount) {
-          $("#{id}").update(amount);
-          sortList();
-          new Effect.Highlight("#{id}");
-        }
-      });  
-    </script>
-    JAVASCRIPT
-  end
-end
